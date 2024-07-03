b0c7e6b42f41375d665315838f139c0cd5d6e07f
haxerr_ <148996577+haxerr9@users.noreply.github.com>
2024-07-03 15:47:45 +0300

    Create install.py

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+
+os.system('pip install configparser')
+os.system('pip install pyftpdlib')
+os.system('pip install ftplib')
+os.system('pip install pyautogui')
+os.system('pip install pyinstaller')
+
+print('[+] SUCCESSFULLY INSTALLED REQUIREMENTS')
